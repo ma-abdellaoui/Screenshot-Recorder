7e567521f45fb6b2bbe5c1554bdba9bb0c4aea0d
ge37wuf <ge37wuf@bitbucket.ase.in.tum.de>
2021-02-14 13:09:30 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Porsche Next TT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(17, 7)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(173, 23)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Prüfstände Überwachung"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Porsche Next TT", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(425, 126)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 29)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Porsche Next TT", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(425, 167)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(93, 29)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Panel1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Porsche Next TT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(325, 136)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox1.Size = New System.Drawing.Size(88, 61)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Status"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Location = New System.Drawing.Point(4, 20)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(79, 32)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.Font = New System.Drawing.Font("Porsche Next TT", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(2, 6)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "..."
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Porsche Next TT", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(13, 98)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(101, 29)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Datei auswählen"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Font = New System.Drawing.Font("Porsche Next TT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 40)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox2.Size = New System.Drawing.Size(505, 54)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Speicherort"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Porsche Next TT", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(4, 22)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(88, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Aktuelle Datei"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.NumericUpDown1)
+        Me.GroupBox3.Font = New System.Drawing.Font("Porsche Next TT", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(13, 134)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(2)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(2)
+        Me.GroupBox3.Size = New System.Drawing.Size(175, 61)
+        Me.GroupBox3.TabIndex = 6
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Refreshrate"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Porsche Next TT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(103, 24)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 23)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Sekunden"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Increment = New Decimal(New Integer() {2, 0, 0, 0})
+        Me.NumericUpDown1.Location = New System.Drawing.Point(8, 23)
+        Me.NumericUpDown1.Margin = New System.Windows.Forms.Padding(2)
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {5, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(90, 31)
+        Me.NumericUpDown1.TabIndex = 7
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {60, 0, 0, 0})
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Porsche Next TT", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(194, 170)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(129, 27)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Mit login starten"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.CheckBox1.Visible = False
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "RemoteTrackingTool"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Porsche Next TT", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(314, 102)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 20)
+        Me.Label5.TabIndex = 2
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Porsche Next TT", 9.749999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(251, 99)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(58, 25)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Stop ->"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Porsche Next TT", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(358, 11)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(156, 29)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "1 Screenshot speichern"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(529, 207)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Remote Client"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,218 @@
+﻿Imports System.IO
+Imports System.Threading
+
+Public Class Form1
+    Public strFileName As String
+    Public Run As Integer
+    Public a As Integer
+    Public RefreshRate As Integer = 60
+    Public Speicherort As String
+    Dim thread As System.Threading.Thread
+    Dim thread2 As System.Threading.Thread
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        CheckForIllegalCrossThreadCalls = False
+        If My.Settings.SpeicherDatei = Nothing Then
+            MessageBox.Show("Kein Speicherort definiert!, Bitte Speicherort auswählen!!")
+        Else
+            strFileName = My.Settings.SpeicherDatei
+            Label2.Text = My.Settings.SpeicherDatei
+            thread2 = New System.Threading.Thread(AddressOf Autostartup)
+            thread2.Start()
+        End If
+        If My.Settings.Autostartup = 1 Then
+            CheckBox1.Checked = True
+        Else
+            CheckBox1.Checked = False
+        End If
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Start Button 
+        If thread2.IsAlive Then
+            Me.Button5.PerformClick()
+        End If
+        If Not IsNothing(strFileName) Then
+            Run = 1
+            thread = New System.Threading.Thread(AddressOf MainFunction)
+            thread.Start()
+        Else
+            MessageBox.Show("Speicheort ist leer!!!, Bitte einen Ort auswählen.")
+        End If
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Run = 0
+        Panel1.BackColor = Color.Red
+        Label4.Text = "Gestoppt"
+        If Not IsNothing(thread) Then
+            thread.Abort()
+        End If
+
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim fd As SaveFileDialog = New SaveFileDialog()
+        'Dim strFileName As String
+        fd.Title = "Bild Speichern"
+        fd.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
+        fd.Filter = "PNG Image|*.png"
+        fd.FilterIndex = 1
+        fd.RestoreDirectory = True
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            strFileName = fd.FileName
+            Label2.Text = fd.FileName
+            My.Settings.SpeicherDatei = fd.FileName
+            My.Settings.Save()
+            MessageBox.Show(fd.FileName)
+        End If
+
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Call TakeScreenshotManual()
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        On Error Resume Next
+        thread2.Abort()
+        Label5.Text = ""
+        Button5.Hide()
+    End Sub
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        If Run = 1 Then
+            MessageBox.Show("Bitte zuerst Aufzeichnung stoppen!")
+        End If
+        RefreshRate = NumericUpDown1.Value
+    End Sub
+    Private Sub MainFunction()
+        If Run = 0 Then
+            Panel1.BackColor = Color.Red
+            thread.Abort()
+        End If
+        While (Run = 1)
+            Panel1.BackColor = Color.Green
+            Label4.Text = "Läuft"
+            TakeScreenshot()
+            Threading.Thread.Sleep(RefreshRate * 1000)
+        End While
+    End Sub
+    Private Sub Autostartup()
+        ' sekunden timer 
+        Dim k As Integer
+        For k = 1 To 15
+            Label5.Text = "Auto Aufzeichnung nach " & 15 - k & " Sek."
+            Threading.Thread.Sleep(1000)
+        Next
+        Me.Label5.Text = ""
+        Me.Button5.Hide()
+        Me.Button1.PerformClick()
+    End Sub
+    Private Sub TakeScreenshotManual()
+        On Error Resume Next
+
+        Dim mystring As String = System.IO.Path.GetFileName(strFileName) & " " & System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+        Dim myFont As Font = New Font("Arial", 50, FontStyle.Bold, GraphicsUnit.Pixel)
+        Dim screenSize As Size = New Size(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Using screenGrab As New Bitmap(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+            'copy the screen to the image
+            Using g = Graphics.FromImage(screenGrab)
+                g.CopyFromScreen(New Point(0, 0), New Point(0, 0), screenSize)
+                g.DrawString(mystring, myFont, Brushes.Red, 0, 0)
+            End Using
+            'save the image
+            If Not strFileName = "" Then
+                Dim tempdatei As String
+                tempdatei = InputBox("Bitte eine Name für das Bild eingeben!, -OHNE Leerzeichen-", "Fehler quittung")
+                If Not IsNothing(tempdatei) Then
+                    screenGrab.Save(System.IO.Path.GetDirectoryName(strFileName) & "\" & tempdatei & ".png", System.Drawing.Imaging.ImageFormat.Png)
+                Else
+                    MessageBox.Show("Vorgang Abgebrochen, da keine Name eingegeben!")
+                End If
+
+            End If
+        End Using
+    End Sub
+    Private Sub TakeScreenshot()
+        On Error Resume Next
+        Dim mystring As String = System.IO.Path.GetFileName(strFileName) & " " & System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
+        Dim myFont As Font = New Font("Arial", 50, FontStyle.Bold, GraphicsUnit.Pixel)
+        Dim screenSize As Size = New Size(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+        Using screenGrab As New Bitmap(My.Computer.Screen.Bounds.Width, My.Computer.Screen.Bounds.Height)
+            'copy the screen to the image
+            Using g = Graphics.FromImage(screenGrab)
+                g.CopyFromScreen(New Point(0, 0), New Point(0, 0), screenSize)
+                g.DrawString(mystring, myFont, Brushes.Red, 0, 0)
+            End Using
+            'save the image
+            If Not strFileName = "" Then
+                screenGrab.Save(strFileName, System.Drawing.Imaging.ImageFormat.Png)
+                '####### ADD Date to picture: 
+            Else
+                MessageBox.Show("Speicherort ist leer!")
+                thread.Abort()
+            End If
+        End Using
+    End Sub
+
+
+
+    Private Function CreateShortCut(ByVal TargetName As String, ByVal ShortCutPath As String, ByVal ShortCutName As String) As Boolean
+        Dim oShell As Object
+        Dim oLink As Object
+        'you don’t need to import anything in the project reference to create the Shell Object
+        Try
+            oShell = CreateObject("WScript.Shell")
+            oLink = oShell.CreateShortcut(ShortCutPath & "\" & ShortCutName & ".lnk")
+
+            oLink.TargetPath = TargetName
+            oLink.WindowStyle = 1
+            oLink.Save()
+        Catch ex As Exception
+        End Try
+        Return True
+    End Function
+    Private Function RemoveShortCut() As Boolean
+        Try
+            My.Computer.FileSystem.DeleteFile(Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\RemotetrackingClient.lnk")
+        Catch ex As Exception
+        End Try
+        Return True
+    End Function
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        Me.Show()
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.WindowState = FormWindowState.Normal
+        NotifyIcon1.Visible = False
+    End Sub
+
+    Private Sub Form1_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Me.Hide()
+            NotifyIcon1.Visible = True
+        End If
+
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Button2.PerformClick()
+        MessageBox.Show("Aufzeichnung gestoppt!")
+    End Sub
+
+    Private Sub CheckBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles CheckBox1.MouseDown
+
+    End Sub
+
+    Private Sub CheckBox1_MouseClick(sender As Object, e As MouseEventArgs) Handles CheckBox1.MouseClick
+        If CheckBox1.Checked = True Then
+            a = CreateShortCut(Environment.GetFolderPath(Environment.SpecialFolder.Programs) & "\Mohamed Alaeddin Abdellaoui\RemoteTrackingClient.appref-ms", Environment.GetFolderPath(Environment.SpecialFolder.Startup), "RemoteTrackingClient")
+            MessageBox.Show("Programm startet bei der Anmeldung automatisch")
+            My.Settings.Autostartup = 1
+            My.Settings.Save()
+
+        Else
+            a = RemoveShortCut()
+            My.Settings.Autostartup = 0
+            My.Settings.Save()
+            MessageBox.Show("Programm startet nicht mehr automatisch!")
+        End If
+    End Sub
+End Class
